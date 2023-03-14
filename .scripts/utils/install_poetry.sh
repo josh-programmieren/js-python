2112143115d65fbf69a9dbdc185948cc48b8b4a9
josh-programmieren <87899799+josh-programmieren@users.noreply.github.com>
2023-03-14 18:46:44 +0100

    removed .scripts folder
    This folder is no longer needed.

--- .scripts/utils/install_poetry.sh
+++
@@ -1,8 +0,0 @@
-if [[ ! -x "`which poetry 2>/dev/null`" ]]
-then
-    read -p "You do not have poetry installed. Install now? (y/N)" choice
-    case "$choice" in 
-        y|Y ) python -m pip install poetry;;
-        n|N|* ) echo "Aborted"; exit 1 ;;
-    esac
-fi
